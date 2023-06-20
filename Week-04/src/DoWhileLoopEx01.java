1df7effc75c80b2ea530380cc44602a76f5899f6
Cabir <cabirbudak@outlook.com>
2023-06-20 09:38:18 -0400

    Java ilk kod

---
+++ Week-04/src/DoWhileLoopEx01.java
@@ -0,0 +1,22 @@
+public class DoWhileLoopEx01 {
+    public static void main(String[] args) {
+        int startValue = 0;
+
+        while(startValue <= 10) {
+            System.out.println(startValue);
+            startValue++;
+
+            System.out.println("while dongusu bitti");
+
+            System.out.println("for dongusu basliyor");
+
+
+        }
+
+        for (int i=0; i<=10; i++) {
+            System.out.println(i);
+        }
+        System.out.println("for dongusu bitti");
+
+    }
+}
